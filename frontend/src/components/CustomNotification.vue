76997efa206d52bf39ddd58d6d8dcdceab7d1e25
workmusicalflow <studioabidjanpro1@gmail.com>
2025-03-31 06:26:36 +0000

    Replace Quasar components with native HTML/CSS in notification component

--- frontend/src/components/CustomNotification.vue
+++ frontend/src/components/CustomNotification.vue
@@ -10,17 +10,22 @@
       :class="[`bg-${color}`, `text-${textColor}`]"
     >
       <div class="custom-notification-content">
-        <q-icon :name="icon" size="md" class="q-mr-sm" />
+        <div class="icon-wrapper q-mr-sm">
+          <span v-if="icon === 'error'" class="material-icons">error</span>
+          <span v-else-if="icon === 'warning'" class="material-icons">warning</span>
+          <span v-else-if="icon === 'info'" class="material-icons">info</span>
+          <span v-else-if="icon === 'check_circle'" class="material-icons">check_circle</span>
+          <span v-else class="material-icons">{{ icon }}</span>
+        </div>
         <span>{{ message }}</span>
       </div>
-      <q-btn
-        flat
-        round
-        dense
-        icon="close"
+      <button
+        class="close-button"
         :class="`text-${textColor}`"
         @click="hide"
-      />
+      >
+        <span class="material-icons">close</span>
+      </button>
     </div>
   </transition>
 </template>
@@ -120,6 +125,46 @@ defineExpose({
   align-items: center;
 }
 
+.icon-wrapper {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 24px;
+  height: 24px;
+}
+
+.close-button {
+  background: transparent;
+  border: none;
+  cursor: pointer;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  padding: 4px;
+  border-radius: 50%;
+}
+
+.close-button:hover {
+  background-color: rgba(255, 255, 255, 0.1);
+}
+
+.material-icons {
+  font-family: 'Material Icons';
+  font-weight: normal;
+  font-style: normal;
+  font-size: 24px;
+  line-height: 1;
+  letter-spacing: normal;
+  text-transform: none;
+  display: inline-block;
+  white-space: nowrap;
+  word-wrap: normal;
+  direction: ltr;
+  -webkit-font-smoothing: antialiased;
+}
+
 /* Import animate.css classes for transitions */
 @import 'https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css';
+/* Import Material Icons */
+@import 'https://fonts.googleapis.com/icon?family=Material+Icons';
 </style>
